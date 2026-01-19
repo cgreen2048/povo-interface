d64e2b5fe7b1e93b0cebca8533d0ba11cb508f86
clair-green2048 <clair.green2048@gmail.com>
2026-01-18 20:03:31 -0500

    search algorithm via regex extraction mostly complete

---
+++ src/lib/courseAbbreviations.ts
@@ -0,0 +1,15 @@
+export const courseAbbreviations = new Set([
+    "ACCT", "ACMS", "ACTG", "AFST", "AL", "ALHN", "ALSF", "AME", "AMST", "ANTH",
+    "ARCH", "ARHI", "ARST", "AS", "ASIA", "BAAL", "BAEG", "BASC", "BAUG", "BES",
+    "BIOS", "BPHY", "CBE", "CDT", "CE", "CHEM", "CHR", "CLAS", "CLGR", "CLLA",
+    "CNST", "CSAL", "CSE", "CSEM", "CSLC", "CST", "DESN", "DMA", "DS", "EALC",
+    "EALJ", "EALK", "ECON", "EDU", "EE", "EG", "ENER", "ENGL", "ESS", "ESTM",
+    "EURO", "FIN", "FTT", "FYS", "GE", "GEST", "GH", "GLAF", "GRED", "GSC",
+    "HESB", "HHS", "HIST", "HPS", "IBMS", "IIPS", "ILS", "IRLL", "IRST", "ISS",
+    "ITAO", "JED", "KSGA", "LAW", "LLEA", "LLRO", "MARK", "MATH", "MBA", "MBAE",
+    "MDMK", "MDSC", "MEAR", "MELC", "MGA", "MGTO", "MI", "MNAE", "MNAR", "MSA", // masters
+    "MSBR", "MSDM", "MSFR", "MSL", "MSM", "MSMG", "MUS", "NSBH", "NSCI", "PCAY",
+    "PHIL", "PHYS", "PLS", "POLS", "PPE", "PRL", "PS", "PSY", "RE", "REG", "ROFR",
+    "ROIT", "ROPO", "ROSP", "RU", "SACM", "SC", "SCGE", "SCPP", "SEI", "SLAV",
+    "SMAC", "SOC", "SOCO", "STV", "SUS", "SUSD", "THEO", "WR",
+]);
--- src/lib/searchAlgorithm.ts
+++ src/lib/searchAlgorithm.ts
@@ -1,20 +1,6 @@
 import { Course, Section, DayCode, dayAliases, DAY_ALIAS_RE } from './types';
-
-const courseAbbrevs = [
-    "ACCT", "ACMS", "ACTG", "AFST", "AL", "ALHN", "ALSF", "AME", "AMST", "ANTH",
-    "ARCH", "ARHI", "ARST", "AS", "ASIA", "BAAL", "BAEG", "BASC", "BAUG", "BES",
-    "BIOS", "BPHY", "CBE", "CDT", "CE", "CHEM", "CHR", "CLAS", "CLGR", "CLLA",
-    "CNST", "CSAL", "CSE", "CSEM", "CSLC", "CST", "DESN", "DMA", "DS", "EALC",
-    "EALJ", "EALK", "ECON", "EDU", "EE", "EG", "ENER", "ENGL", "ESS", "ESTM",
-    "EURO", "FIN", "FTT", "FYS", "GE", "GEST", "GH", "GLAF", "GRED", "GSC",
-    "HESB", "HHS", "HIST", "HPS", "IBMS", "IIPS", "ILS", "IRLL", "IRST", "ISS",
-    "ITAO", "JED", "KSGA", "LAW", "LLEA", "LLRO", "MARK", "MATH", "MBA", "MBAE",
-    "MDMK", "MDSC", "MEAR", "MELC", "MGA", "MGTO", "MI", "MNAE", "MNAR", "MSA", // masters
-    "MSBR", "MSDM", "MSFR", "MSL", "MSM", "MSMG", "MUS", "NSBH", "NSCI", "PCAY",
-    "PHIL", "PHYS", "PLS", "POLS", "PPE", "PRL", "PS", "PSY", "RE", "REG", "ROFR",
-    "ROIT", "ROPO", "ROSP", "RU", "SACM", "SC", "SCGE", "SCPP", "SEI", "SLAV",
-    "SMAC", "SOC", "SOCO", "STV", "SUS", "SUSD", "THEO", "WR",
-];
+import { courseAbbreviations } from './courseAbbreviations';
+import { courseAttributes } from './courseAttributes';
 
 export function searchAlgorithm(courses: Course[], query: string)  {
     let searchCourses = structuredClone(courses);
@@ -23,12 +9,10 @@ export function searchAlgorithm(courses: Course[], query: string)  {
         .replace(DAY_ALIAS_RE, match => dayAliases[match]) // replace day aliases
         .replace(/\s+/g, ' ')         // normalize spaces
         .trim();
-    normalizedQuery = normalizeTimeDashes(normalizedQuery);
     
     // This section handles HARD CONSTRAINTS EXTRACTED VIA REGEX
 
     // For the future, switch to matchALl to parse between CRNs and course numbers
-    const abbrevsRe = courseAbbrevs.join('|');
     // const forbiddenPrefix = `(?:${abbrevsRe})[\\s:-]*`;         // ?: is non-capturing group to grab course abbrev but don't save for memory
     // const crnPattern = `(?<!${forbiddenPrefix})\\b\\d{5}`;      // neg lookbehind to ensure no abbrev before CRN (\b\d{5}) with ?<!pattern
     // const crnRegex = new RegExp(crnPattern, 'g');               // g for global
@@ -41,75 +25,63 @@ export function searchAlgorithm(courses: Course[], query: string)  {
             .some(section => crnMatches.includes(section.crn.toString())));
     }
 
-    const courseAbbrevPlusNumberPattern = `\\b(?:${abbrevsRe})[\\s:-]*\\d{5}\\b`;
-    const courseAbbrevPlusNumberRegex = new RegExp(courseAbbrevPlusNumberPattern, 'g');
-    const courseAbbrevPlusNumberMatches = normalizedQuery.match(courseAbbrevPlusNumberRegex);
-
-    if (courseAbbrevPlusNumberMatches) {
-        const normalizedCourseCodes = courseAbbrevPlusNumberMatches.map(match =>
-            match.replace(/[\s:-]+/g, " ").trim()
-        );
+    const courseAbbrevPlusNumberMatches = extractCourseAbbrevPlusNumbers(normalizedQuery);
+    if (courseAbbrevPlusNumberMatches.length > 0) {
+        const filteredMatchesByAbbrev = courseAbbrevPlusNumberMatches.filter(code => {
+            const abbrev = code.split(" ")[0];
+            return courseAbbreviations.has(abbrev);
+        });
 
         searchCourses = searchCourses.filter(course => {
             const code = `${course.abbreviation.toUpperCase()} ${course.number}`; 
-            return normalizedCourseCodes.includes(code);
+            return filteredMatchesByAbbrev.includes(code);
         });
     }
 
 
     // Require at least one day code in first group, then in additional groups, match subsequent days w/priority given to Th
     const dayMatches = normalizedQuery.match(/\b(?:TH|[MTWF])(?:\s*(?:TH|[MTWF]))*\b/g);
-    if (dayMatches) {
-        const days = findQueryDays(dayMatches);
-
-        // Find all courses that contains at least one section that has all days in the query
-        searchCourses = searchCourses.filter((course: Course) =>
-            Object.values(course.sections).some((section: Section) =>
-            days.every(d => section.days.includes(d))
-            )
-        );
-    }
+    const days = dayMatches ? findQueryDays(dayMatches) : [];
 
     const timeRangeMatches = normalizedQuery.match(TIME_RANGE_RE);
-    if (timeRangeMatches) {
-        searchCourses = searchCourses.filter(course =>
-            Object.values(course.sections).some(section =>
-            timeRangeMatches.includes(section.time)
-            )
-        );
-    }
 
-    const timeStartMatches = normalizedQuery.match(TIME_START_RE);
-    if (timeStartMatches) {
-        searchCourses = searchCourses.filter((course: Course) => {
-            return Object.values(course.sections).some((section: Section) => {
-                return timeStartMatches.includes(section.time);
-            })
-        })
-    }
+    // const timeStartMatches = normalizedQuery.match(TIME_START_RE);
+    // if (timeStartMatches) {
+    //     searchCourses = searchCourses.filter((course: Course) => {
+    //         return Object.values(course.sections).some((section: Section) => {
+    //             return timeStartMatches.includes(section.time);
+    //         })
+    //     })
+    // }
 
     const credits = extractCreditsFromQuery(normalizedQuery);
-    if (credits.length > 0) {
-        searchCourses = searchCourses.filter(course =>
-            Object.values(course.sections).some(section =>
-            credits.includes(section.credits)
-            )
-        );
-    }
 
     // Add full list of attributes to filter out matches that aren't in the attributes (ex. "THAT")
-    const fulfilledRequirementsMatches = normalizedQuery.match(/\b[A-Z]{4}\b/g);
-    if (fulfilledRequirementsMatches) {
-        searchCourses = searchCourses.filter((course: Course) => 
-            Object.values(course.sections).some((section: Section) => 
-            section.fulfilledRequirements.some(req => fulfilledRequirementsMatches.includes(req)))
-        );
+    const rawAttributeMatches = normalizedQuery.match(/\b[A-Z0-9]{2,4}\b/g);
+    const filteredAttributeMatches = rawAttributeMatches ? rawAttributeMatches.filter(attr => courseAttributes.has(attr)) : [];
+
+    const q = {
+        days: days ?? [],
+        timeRanges: timeRangeMatches ?? [],
+        credits: credits ?? [],
+        attributes: filteredAttributeMatches ?? []
     }
+    searchCourses = searchCourses.filter((course: Course) =>
+        Object.values(course.sections).some((section: Section) =>
+            sectionSatisfiesAllMatches(section, q)
+        )
+    );
 
 
     // This section handles similarity search that represents SOFT CONSTRAINTS/KEYWORDS
 
     return searchCourses;
+}
+
+const extractCourseAbbrevPlusNumbers = (query: string): string[] => {
+    const courseAbbrevPlusNumberPattern = /\b([A-Z]{2,4})[\s:-]*(\d{5})\b/gi;
+    const matches = [...query.matchAll(courseAbbrevPlusNumberPattern)];
+    return matches.map(match => `${match[1]} ${match[2]}`);
 }
 
 const findQueryDays = (daysStrings: string[]): DayCode[] => {
@@ -136,7 +108,7 @@ const extractCreditsFromQuery = (query: string): number[] => {
     const credits = new Set<number>();
 
     // Handles "3 credits", "3 or 4 credits", "3/4 credits", "3-4 credits", etc.
-    const creditsRe = /\b(?:(?<a>[1-9]|1[0-9])\s*(?:OR|\/|-)\s*)?(?<b>[1-9]|1[0-9])\s*(?:CREDITS|CREDIT|CR|CRED)[\.\?]\b/gi;
+    const creditsRe = /\b(?:(?<a>[1-9]|1[0-9])\s*(?:OR|\/|-)\s*)?(?<b>[1-9]|1[0-9])\s*(?:CREDITS|CREDIT|CR|CRED)(?:[\.\?])?\b/gi;
 
     for (const match of query.matchAll(creditsRe)) {
         const a = match.groups?.a ? Number(match.groups.a) : null;
@@ -151,20 +123,41 @@ const extractCreditsFromQuery = (query: string): number[] => {
     return [...credits];
 }
 
-const TIME_TOKEN = /\d{1,2}(?::\d{2})?(?:am|pm)?/;
+const TIME_TOKEN = /\d{1,2}(?::\d{2})?(?:AM|PM)?/;
 
 const TIME_RANGE_RE = new RegExp(
     `\\b(${TIME_TOKEN.source})-(${TIME_TOKEN.source})\\b`,
     "gi"
 );
 
-const TIME_START_RE = new RegExp(
-    `\\b(${TIME_TOKEN.source})\\b`,
-    "gi"
-);
+// const TIME_START_RE = new RegExp(
+//     `\\b(${TIME_TOKEN.source})\\b`,
+//     "gi"
+// );
 
-function normalizeTimeDashes(query: string) {
-    return query.replace(TIME_RANGE_RE, "$1-$2");
+const sectionSatisfiesAllMatches = (section: Section, q: {
+    days: DayCode[],
+    timeRanges: string[],
+    credits: number[],
+    attributes: string[]
+}): boolean => {
+
+    if (q.days.length > 0 && q.days.some(day => !section.days.includes(day))) {
+        return false;
+    }
+
+    if (q.timeRanges.length > 0 && !q.timeRanges.includes(section.time.toUpperCase())) {
+        return false;
+    }
+
+    if (q.credits.length > 0 && !q.credits.includes(section.credits)) {
+        return false;
+    }
+
+    if (q.attributes.length > 0 && !section.fulfilledRequirements.some(attr => q.attributes.includes(attr))) {
+        return false;
+    }
+
+    return true;
 }
-
 
